6a7afeab4bd468b065dfc1b6b89f025be817d15e
Abel <abel.amy0@gmail.com>
2025-12-08 19:15:03 -0500

    Add Validator class

---
+++ app/Validator.py
@@ -0,0 +1,15 @@
+class Validator:
+    def validate(self, record):
+        pass
+
+    def validateHealthCardNumber(self, healthCardNumber):
+        pass
+
+    def validateVersionCode(self, versionCode):
+        pass
+
+    def validateDateOfBirth(self, dateOfBirth):
+        pass
+
+    def validateServiceDate(self, serviceDate):
+        pass
