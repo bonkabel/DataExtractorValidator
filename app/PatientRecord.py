fda9d978bb2bf01e17e4b4298bfcf419fd92ea24
Abel <abel.amy0@gmail.com>
2025-12-08 20:20:19 -0500

    Fix PatientRecord docstring

--- app/PatientRecord.py
+++ app/PatientRecord.py
@@ -1,15 +1,15 @@
-'''
-Represents a single patient record
-
-Attributes:
-    patientId: Unique identifier for the patient
-    healthCardNumber: Ten digit Ontario health card number. Must pass Luhn algo check
-    versionCode: Two letter (uppercase) health card version code
-    dateOfBirth: Patient date of birth in YYYY-MM-DD format
-    serviceDate: Date the service was provided in YYYY-MM-DD format
-
-'''
 class PatientRecord:
+    '''
+    Represents a single patient record
+
+    Attributes:
+        patientId: Unique identifier for the patient
+        healthCardNumber: Ten digit Ontario health card number. Must pass Luhn algo check
+        versionCode: Two letter (uppercase) health card version code
+        dateOfBirth: Patient date of birth in YYYY-MM-DD format
+        serviceDate: Date the service was provided in YYYY-MM-DD format
+
+    '''
     def __init__(self, patientId, healthCardNumber, versionCode, dateOfBirth, serviceDate):
         self.patientId = patientId
         self.healthCardNumber = healthCardNumber
