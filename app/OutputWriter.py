baca3ff711c1db0841c74f49684e4e0b0db5328c
Abel <abel.amy0@gmail.com>
2025-12-23 10:31:22 -0500

    Refactor OutputWriter, Add writeJSON

--- app/OutputWriter.py
+++ app/OutputWriter.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
-
 from app.Fields import Fields
+import json
 
 
 class OutputWriter:
@@ -12,11 +12,23 @@ class OutputWriter:
 
     Only responsible for formatting and writing.
     """
-    def writeValidCSV(self, records, path):
+
+    def __init__(self, validRecords, invalidRecords):
+        self.validRecords = validRecords
+        self.invalidRecords = invalidRecords
+        self.totalRecords = len(validRecords) + len(invalidRecords)
+        self.ruleStats = {}
+        self.fieldStats = {}
+
+        for record, errors in invalidRecords:
+            for e in errors:
+                self.ruleStats[e.rule] = self.ruleStats.get(e.rule, 0) + 1
+                self.fieldStats[e.field] = self.fieldStats.get(e.field, 0) + 1
+
+    def writeValidCSV(self, path):
         """
         Writes the valid patient records to a csv file
 
-        :param records: List of valid PatientRecord objects
         :param path: Output file path for the csv file
         """
         with open(path, "w", newline="") as f:
@@ -31,7 +43,7 @@ class OutputWriter:
             ])
 
             # Write the actual records
-            for record in records:
+            for record in self.validRecords:
                 writer.writerow([
                     record.patientId,
                     record.healthCardNumber,
@@ -40,7 +52,7 @@ class OutputWriter:
                     record.serviceDate
                 ])
 
-    def writeErrorReport(self, invalidRecords, path, validCount):
+    def writeErrorReport(self, path):
         """
         Write a text report describing the invalid records and their errors.
 
@@ -48,51 +60,53 @@ class OutputWriter:
             - Timestamp of generation
             - Summary of total, valid and invalid records
             - List of invalid records with error messages
-
-        :param invalidRecords: List of tuples (record, errors)
-        :param path: Output file path for the report file
-        :param validCount: Number of valid records
         """
-
-        totalProcessed = validCount + len(invalidRecords)
-
-        # Set up the error statistics
-        ruleStats = {}
-        fieldStats = {}
-        for record, errors in invalidRecords:
-            for e in errors:
-                ruleStats[e.rule] = ruleStats.get(e.rule, 0) + 1
-                fieldStats[e.field] = fieldStats.get(e.field, 0) + 1
-
-
 
         with open(path, "w") as f:
             f.write("Summary\n")
             f.write("============\n")
             f.write(f"Generated: {datetime.now()}\n")
-            f.write(f"Total Records Processed: {totalProcessed}\n")
-            f.write(f"Valid Records: {validCount}\n")
-            f.write(f"Invalid Records: {len(invalidRecords)}\n")
-            f.write(f"Percent of records valid: {validCount / totalProcessed * 100}%\n\n")
+            f.write(f"Total Records Processed: {self.totalRecords}\n")
+            f.write(f"Valid Records: {self.validRecords}\n")
+            f.write(f"Invalid Records: {len(self.invalidRecords)}\n")
+            f.write(f"Percent of records valid: {len(self.validRecords) / self.totalRecords * 100}%\n\n")
 
             f.write(f"Validation Issues\n")
             f.write(f"=================\n")
-            for rule, count in ruleStats.items():
+            for rule, count in self.ruleStats.items():
                 f.write(f"{rule}: {count}\n")
             f.write("\n")
 
             f.write(f"What fields had the issues\n")
             f.write(f"==========================\n")
-            for field, count in fieldStats.items():
+            for field, count in self.fieldStats.items():
                 f.write(f"{Fields.getDisplayName(field)}: {count}\n")
             f.write("\n")
 
             f.write("Invalid Records\n")
             f.write("===============\n")
             # Write each invalid record with its error message
-            for record, errors in invalidRecords:
+            for record, errors in self.invalidRecords:
                 f.write(f"Patient ID: {record.patientId}\n")
 
                 for error in errors:
                     f.write(f"\t- {error.message}\n")
                 f.write("\n")
+
+    def writeJSON(self, path):
+        """Writes to a JSON file"""
+
+        stats = {
+            "summary": {
+                "timestamp": datetime.now().isoformat(),
+                "totalRecordsProcessed": self.totalRecords,
+                "validRecords": len(self.validRecords),
+                "invalidRecords": len(self.invalidRecords),
+                "percentRecordsValid": len(self.validRecords) / self.totalRecords * 100
+            },
+            "validationIssues": self.ruleStats,
+            "fieldsWithIssues": self.fieldStats
+        }
+
+        with open(path, "w") as f:
+            json.dump(stats, f, indent=4)
