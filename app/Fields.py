d2bb78c6f08d2c83d2c2af8ff5b32a2a9b5b8d77
Abel <abel.amy0@gmail.com>
2025-12-23 10:30:25 -0500

    Refactor Fields and RUles

--- app/Fields.py
+++ app/Fields.py
@@ -9,15 +9,6 @@ class Fields:
     DATE_OF_BIRTH = "dateofbirth"
     SERVICE_DATE = "servicedate"
 
-    ALL_FIELDS = {
-        PATIENT_ID,
-        HEALTH_CARD_NUMBER,
-        VERSION_CODE,
-        DATE_OF_BIRTH,
-        SERVICE_DATE,
-    }
-
-
     DISPLAY_NAME = {
         PATIENT_ID: "Patient ID",
         HEALTH_CARD_NUMBER: "Health Card Number",
@@ -26,12 +17,20 @@ class Fields:
         SERVICE_DATE: "Service Date",
     }
 
-    @staticmethod
-    def getDisplayName(field):
+    @classmethod
+    def getDisplayName(cls, fieldName):
         """
         Returns the display name of a field.
 
-        :param field: The field name
+        :param fieldName: The field name
         :return:
         """
-        return Fields.DISPLAY_NAME.get(field, field)
+        return Fields.DISPLAY_NAME.get(fieldName, fieldName)
+
+    @classmethod
+    def getAllFields(cls):
+        """
+
+        :return: A list of all fields.
+        """
+        return set(cls.DISPLAY_NAME.keys())
--- app/Rules.py
+++ app/Rules.py
@@ -5,14 +5,14 @@ class Rules:
     """
 
     # A required field is empty or not present
-    MISSING = "missing field"
+    MISSING = "missingfield"
 
     # A field has a value but it is not logically valid
     # E.g. failed luhn check, invalid date, bad characters, etc.
-    INVALID = "invalid value"
+    INVALID = "invalidvalue"
 
     # A value is outside the allowed range
-    RANGE = "value outside range"
+    RANGE = "valueoutsiderange"
 
     # The value exists but it is of the wrong type
     # E.g. String instead of int
@@ -21,3 +21,28 @@ class Rules:
     # The record structure is incorrect
     # E.g. Wrong number of fields, missing columns, malformed data
     STRUCTURE = "structure"
+
+    DISPLAY_NAME = {
+        MISSING: "Missing Field",
+        INVALID: "Invalid Field",
+        RANGE: "Value Outside Range",
+        TYPE: "Type",
+        STRUCTURE: "Structure"
+    }
+
+    @classmethod
+    def getDisplayName(cls, ruleName):
+        """
+        Returns the display name of a rule.
+
+        :param ruleName: The rule name
+        :return:
+        """
+        return Rules.DISPLAY_NAME.get(ruleName, ruleName)
+
+    @classmethod
+    def getAllFields(cls):
+        """
+        :return: A list of all fields.
+        """
+        return set(cls.DISPLAY_NAME.keys())
